fe3707afe8280bb117356c6c5e8140ddf8feb49b
RSheng <1983676094@qq.com>
2021-11-28 09:13:32 -0800

    interrupt

--- boot.asm
+++ boot.asm
@@ -1,5 +1,8 @@
 [org 0x7c00]
 
+PIC_M_CMD equ 0x20
+PIC_M_DATA equ 0x21
+
 mov ax, 3
 int 0x10 ;将显示模式设置成文本模式（清频）
 
@@ -10,11 +13,26 @@ mov sp, 0x7c00; 初始化堆栈
 
 xchg bx, bx
 
+;注册
+mov word [8 * 4], clock
+mov word [8 * 4 + 2], 0
+
+;写入屏蔽字
+mov al, 0b1111_1110
+out PIC_M_DATA, al
+
+;状态字 IF = 1
+sti
+
 loopa:
 	mov bx, 3
 	mov al, 'A'
 	call blink
 	jmp loopa
+	
+clock:
+	xchg bx, bx
+	iret
 
 blink:
 		push es
